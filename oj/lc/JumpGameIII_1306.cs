368f026af87ac646e476ac8f44adfd660e6f65b8
jahid <jahid96.net@gmail.com>
2026-05-18 20:41:59 +0600

    Daily challenge - 1306

---
+++ oj/lc/JumpGameIII_1306.cs
@@ -0,0 +1,21 @@
+﻿namespace oj.lc;
+
+public class JumpGameIII_1306
+{
+    public bool CanReach(int[] arr, int start)
+    {
+
+        if (start < 0 || start > arr.Length - 1 || arr[start] == -1)
+            return false;
+
+        if (arr[start] == 0)
+            return true;
+
+        int jump = arr[start];
+
+        arr[start] = -1;
+
+        return CanReach(arr, start + jump) || CanReach(arr, start - jump);
+
+    }
+}
