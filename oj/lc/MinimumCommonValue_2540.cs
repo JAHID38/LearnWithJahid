e82884d7508a36c4ac037775ffce299dffe4c57c
Jahid_FS <Jaislam@finsource.local>
2026-05-19 12:38:43 +0600

    Daily challenge -19May26

---
+++ oj/lc/MinimumCommonValue_2540.cs
@@ -0,0 +1,20 @@
+﻿namespace oj.lc;
+
+internal class MinimumCommonValue_2540
+{
+    public int GetCommon(int[] nums1, int[] nums2)
+    {
+        int i = 0, j = 0;
+
+        while( i < nums1.Length && j < nums2.Length)
+        {
+            if (nums1[i] == nums2[j])
+                return nums1[i];
+
+            _ = nums1[i] > nums2[j] ? ++j : ++i;
+
+        }
+
+        return -1;
+    }
+}
