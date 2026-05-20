1abe38c781c5c089eac8630a6d6470393016b427
Jahid_FS <Jaislam@finsource.local>
2026-05-20 15:37:39 +0600

    LC Daily challenge -20May26

---
+++ oj/lc/FindthePrefixCommonArrayofTwoArrays_2657.cs
@@ -0,0 +1,27 @@
+﻿namespace oj.lc;
+
+public class FindthePrefixCommonArrayofTwoArrays_2657
+{
+    public int[] FindThePrefixCommonArray(int[] A, int[] B)
+    {
+        int[] ans = new int[A.Length];
+        int[] freq = new int[A.Length + 1];
+
+        int count = 0;
+
+        for (int i = 0; i < A.Length; i++)
+        {
+            freq[A[i]]++;
+            freq[B[i]]++;
+
+            if (freq[A[i]] > 1) count++;
+            if (freq[B[i]] > 1) count++;
+
+            if (A[i] == B[i]) count--;
+
+            ans[i] = count;
+        }
+
+        return ans;
+    }
+}
